d11f33a3af7bf07dd268138abbb27e6511ede6cf
unknown <frferdinand22@gmail.com>
2024-03-06 20:34:40 +0700

    added level 2

--- scenes/WinArea.gd
+++ scenes/WinArea.gd
@@ -4,6 +4,6 @@ export var sceneName: String = "Level 1"
 
 func _on_WinArea_body_entered(body):
 	if body.get_name() == "Player":
-		get_tree().change_scene(str("res://scenes/" + "WinScreen" + ".tscn"))
+		get_tree().change_scene(str("res://scenes/" + "Level2" + ".tscn"))
 
 
